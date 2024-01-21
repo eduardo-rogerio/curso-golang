abdbf122b96f779e958372d1c4550424ee29099c
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2024-01-20 23:56:49 -0300

    aula86

---
+++ teste/tabela/string_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "test"
+
+const msgIndex = "%s (parte: %s) - Índices: esperado (%d) <> encontrado (%d)"
+
+func TestIndex(t *testing.T) {
+  testes := []struct {
+    texto string
+    parte string
+    esperado int
+  }{
+    {"Cod3r é show", "Cod3r", 0}
+    {"", "", 0}
+    {"Opa", "opa", -1}
+    {"leonardo", "o", 2}
+  }
+
+  for _,teste := range testes{
+    t.Logf("Massa: %v", teste)
+  }
+}
